78e36bb26b36f77716e6b74aa7e5bb805ba93943
wenhainan <whndeweilai@163.com>
2022-03-30 16:51:40 +0800

    初始化提交

---
+++ README.md
@@ -0,0 +1,26 @@
+## FileCache
+-  轻量级,无依赖的本地文件缓存扩展
+## 安装 
+```shell
+composer require  wenhainan/FileCache
+```
+
+## 如何使用
+```php
+        use wenhainan\FileCache;
+        //例如 生成一个随机token字符串  这个是自带的一个工具 
+        $config = [
+            'expire'=>3600,      //expire time , unit seconds
+            'path' =>'./cache'  //self dir
+        ];
+        $cache = new FileCache();
+        //设置缓存
+        $cache->set('key','value',60);   //set key = value  expire = 60s
+        //获取缓存
+        $cache->get('key');
+        //删除缓存
+        $cache->del('key');  
+```
+
+## 配置要求
+- PHP 5.6+
---
+++ src/FileCache.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace think\wenhainan;
+
+/**
+ * FileCache操作类
+ */
+class FileCache
+{
+    CONST PATH = (PATH_SEPARATOR==';'?'c:/tmp/filecache':'/tmp/filecache' );
+    protected $options = [
+        'expire'        => 3600,
+        //末尾不要加斜杠
+        'path'          =>  self::PATH
+    ];
+
+    // 构造函数
+    public function __construct($options = [])
+    {
+        if (!empty($options)) {
+            $this->options = array_merge($this->options, $options);
+        }
+        $this->init();
+    }
+
+    // 初始化检查
+    private function init()
+    {
+        // 创建项目缓存目录
+        if (!is_dir($this->options['path'])) {
+            mkdir($this->options['path'], 0777, true);
+        }
+    }
+
+    // 获取存储的数据
+    public function get($name, $default = false)
+    {
+        $filename = $this->getCacheKey($name);
+        if (!is_file($filename)) {
+            return $default;
+        }
+        $content = file_get_contents($filename);
+        if (false !== $content) {
+            $arr = json_decode($content,true);
+            if ($arr['expire'] <= time()) {
+                return false;
+            }
+            return $arr['data'];
+        }
+    }
+
+    // 设置存储的数据
+    public function set($name, $value, $expire = null)
+    {
+        if (is_null($expire)) {
+            $expire = $this->options['expire'];
+        }
+        $filename = $this->getCacheKey($name);
+        $json = json_encode(['data' => $value, 'expire' => time() + $expire]);
+        $result = file_put_contents($filename,$json);
+        if ($result) {
+            return true;
+        }
+        return false;
+    }
+
+    // 删除存储的数据
+    public function del($name)
+    {
+        $filename = $this->getCacheKey($name);
+        @unlink($filename);
+    }
+
+    // 获取缓存文件名
+    public function getCacheKey($name)
+    {
+        $file_path = $this->options['path']."/".$name.'_cache.php';
+        @chmod($file_path,0777);
+        return $file_path;
+    }
+}
